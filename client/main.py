2319391c4563f07ace604d61dc707aa56828a57d
gemma-draper <g.l.draper@outlook.com>
2021-09-09 20:45:50 +0100

    Adds basic structure to client script.

---
+++ client/main.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+# GET
+get_response = requests.get()
+
+# POST
+post_response = requests.post()
+
+# PUT
+put_response = requests.put()
+
+# DELETE
+del_response = requests.delete()
