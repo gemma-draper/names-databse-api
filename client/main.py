979d31555784ff99ae2e5c2244e0e03cc259346d
gemma-draper <g.l.draper@outlook.com>
2021-09-09 20:52:08 +0100

    More basic structure.

--- client/main.py
+++ client/main.py
@@ -4,11 +4,20 @@ import json
 # GET
 get_response = requests.get()
 
+
 # POST
-post_response = requests.post()
+name_to_post = {
+    'id': '',
+    'first_name': '',
+    'last_name': ''
+}
+post_response = requests.post(url, data=json.dumps(name_to_post))
 
 # PUT
-put_response = requests.put()
+name_to_update = {}
+put_response = requests.put(url, data=name_to_update)
 
 # DELETE
-del_response = requests.delete()
+name_to_delete = {}
+del_response = requests.delete(url, name_to_delete)
+
